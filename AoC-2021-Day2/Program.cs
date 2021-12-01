5b2054da40dc8f742b49231a5d66a504447e32bc
Family <alistairfrith@gmail.com>
2021-12-01 21:38:14 +0000

    Added day 2 template

---
+++ AoC-2021-Day2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        DoIt("inputExample.txt");
+        DoIt("input.txt");
+    }
+
+    public static void DoIt(string inputFileName)
+    {
+        string[] lines = File.ReadAllLines(inputFileName);
+        int[] depths = Array.ConvertAll(lines, l => int.Parse(l));
+
+        Console.WriteLine($"Processing input from {inputFileName}");
+        Part1(depths);
+        Part2(depths);
+    }
+
+    public static void Part1(int[] depths)
+    {
+        int count = 0;
+        for (int i = 1; i < depths.Length; i++)
+        {
+            if (depths[i - 1] < depths[i])
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine(count);
+    }
+
+    public static void Part2(int[] depths)
+    {
+        List<int> averages = new List<int>();
+        for (int i = 2; i < depths.Length; i++)
+        {
+            averages.Add(depths[i] + depths[i - 1] + depths[i - 2]);
+        }
+
+        Part1(averages.ToArray());
+    }
+}
