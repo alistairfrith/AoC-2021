0a6d4dc5a0bb36efd6b7cd02a37e5318ce4c6259
Family <alistairfrith@gmail.com>
2021-12-04 09:45:11 +0000

    Day 3 done.

--- AoC-2021-Day3/Program.cs
+++ AoC-2021-Day3/Program.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Collections.Generic;
 using System.Reflection;
+using System.Linq;
 
 class Program
 {
@@ -28,11 +29,44 @@ class Program
 
     public static int Part1(string[] lines)
     {
-        return 0;
+        int gamma = 0;
+        int epsilon = 0;
+        for (int i=0; i<lines[1].Length; i++)
+        {
+            int ones = lines.Where(s => s.Substring(i,1) == "1").Count();
+            int zeros = lines.Where(s => s.Substring(i, 1) == "0").Count();
+
+            gamma = (gamma * 2) + ((ones > zeros) ? 1 : 0);
+            epsilon = (epsilon * 2) + ((ones < zeros) ? 1 : 0);
+        }
+        return gamma * epsilon;
     }
 
     public static int Part2(string[] lines)
     {
-        return 0;
+        int o2Rating = GetRating(lines, "1", "0");
+        int co2Rating = GetRating(lines, "0", "1");
+
+        return o2Rating * co2Rating;
+    }
+
+    private static int GetRating(string[] lines, string onesChar, string zerosChar)
+    {
+        string[] rating = lines;
+        for (int i = 0; i < lines[1].Length; i++)
+        {
+            int ones = rating.Where(s => s.Substring(i, 1) == "1").Count();
+            int zeros = rating.Where(s => s.Substring(i, 1) == "0").Count();
+
+            string character = (ones >= zeros) ? onesChar : zerosChar;
+            rating = rating.Where(s => s.Substring(i, 1) == character).ToArray();
+
+            if (rating.Length == 1)
+            {
+                break;
+            }
+        }
+
+        return Convert.ToInt32(rating[0], 2);
     }
 }
---
+++ AoC-2021-Day4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC_2021_Day4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
