ab726dfec9647b8c9fe08a833528f3d6e6380dbc
Family <alistairfrith@gmail.com>
2021-12-04 13:34:56 +0000

    Day4 Part1

---
+++ AoC-2021-Day4/BingoCard.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC_2021_Day4
+{
+    public class BingoCard
+    {
+        private string[,] numbers = new string[5, 5];
+        private bool[,] matches = new bool[5, 5];
+
+        public BingoCard(IEnumerable<string> lines)
+        {
+            string[] separators = { " ", "  " };
+            string[] rows = lines.ToArray();
+            for (int rowNum = 0; rowNum < 5; rowNum++)
+            {
+                var row = rows[rowNum].Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+                for (int colNum = 0; colNum < 5; colNum++)
+                {
+                    numbers[rowNum, colNum] = row[colNum];
+                }
+            }
+
+        }
+
+        override public string ToString()
+        {
+            string result = "";
+
+            for (int rowNum = 0; rowNum < 5; rowNum++)
+            {
+                for (int colNum = 0; colNum < 5; colNum++)
+                {
+                    char matchChar = ' ';
+                    if (matches[rowNum, colNum])
+                    {
+                        matchChar = '*';
+                    }
+                    result += $"  {matchChar}{numbers[rowNum, colNum],2}{matchChar}  ";
+                }
+                result += '\n';
+            }
+
+            return result;
+        }
+       
+        internal int Score(string winningCall)
+        {
+            int sumUnmarked = 0;
+            for (int rowNum = 0; rowNum < 5; rowNum++)
+            {
+                for (int colnum = 0; colnum < 5; colnum++)
+                {
+                    if (matches[rowNum, colnum] == false)
+                    {
+                        sumUnmarked += int.Parse(numbers[rowNum, colnum]);
+                    }
+                }
+            }
+
+            return sumUnmarked * int.Parse(winningCall);
+        }
+
+        internal void Mark(string call)
+        {
+            for (int rowNum = 0; rowNum < 5; rowNum++)
+            {
+                for (int colnum = 0; colnum < 5; colnum++)
+                {
+                    if (numbers[rowNum, colnum] == call)
+                    {
+                        matches[rowNum, colnum] = true;
+                        return;
+                    }
+                }
+            }
+
+            return;
+        }
+
+        internal bool CompleteLine()
+        {
+            for (int rowNum = 0; rowNum < 5; rowNum++)
+            {
+                if (matches[rowNum, 0] && matches[rowNum, 1] && matches[rowNum, 2] && matches[rowNum, 3] && matches[rowNum, 4])
+                    return true;
+            }
+
+            for (int colnum = 0; colnum < 5; colnum++)
+            {
+                if (matches[0, colnum] && matches[1, colnum] && matches[2, colnum] && matches[3, colnum] && matches[4, colnum])
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
--- AoC-2021-Day4/Program.cs
+++ AoC-2021-Day4/Program.cs
@@ -1,15 +1,87 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AoC_2021_Day4
 {
-    internal class Program
+
+    class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            DoIt("inputExample.txt");
+            DoIt("input.txt");
+
+            Console.WriteLine("Hit any key to continue");
+            Console.ReadKey();
+        }
+
+        public static void DoIt(string inputFileName)
+        {
+            string[] lines = File.ReadAllLines(inputFileName);
+
+            Console.WriteLine($"Processing input from {inputFileName}");
+
+            string programName = Assembly.GetExecutingAssembly().FullName.Split(',')[0];
+            Console.WriteLine($"\t{programName} Part 1 : {Part1(lines)}");
+            Console.WriteLine($"\t{programName} Part 2 : {Part2(lines)}");
+            Console.WriteLine();
+        }
+
+        public static int Part1(string[] lines)
+        {
+            string[] calls = lines[0].Split(',');
+
+            var cards = PopulateCards(lines.Skip(2));
+
+            return Play(calls, cards);
+        }
+
+        private static int Play(string[] calls, List<BingoCard> cards)
+        {
+            foreach (string call in calls)
+            {
+                foreach (BingoCard card in cards)
+                {
+                    card.Mark(call);
+                    if (card.CompleteLine())
+                    {
+                        Console.WriteLine(card.ToString());
+                        return card.Score(call);
+                    }
+                }
+
+            }
+
+            return 0;
+        }
+
+        private static void PrintCards(List<BingoCard> cards)
+        {
+            foreach (var card in cards)
+            {
+                Console.WriteLine (card.ToString());
+            }
+        }
+
+        private static List<BingoCard> PopulateCards(IEnumerable<string> lines)
+        {
+            List<BingoCard> cards = new List<BingoCard>();
+
+            while (lines.Count() > 0)
+            {
+                cards.Add(new BingoCard(lines.Take(5)));
+                lines = lines.Skip(6);
+            }
+
+            return cards;
+        }
+
+        public static int Part2(string[] lines)
+        {
+            return 0;
         }
     }
 }
