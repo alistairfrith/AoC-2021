6476803cc8889c6a01971cb0238c21a017b524c3
Family <alistairfrith@gmail.com>
2021-12-08 22:09:49 +0000

    Skeletons added for days 8 and 9

---
+++ AoC-2021-Day9-/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC_2021_Day9_
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            DoIt("inputExample.txt");
+            DoIt("input.txt");
+
+            Console.WriteLine("Hit any key to continue");
+            Console.ReadKey();
+        }
+
+        public static void DoIt(string inputFileName)
+        {
+            string[] lines = File.ReadAllLines(inputFileName);
+
+            Console.WriteLine($"Processing input from {inputFileName}");
+
+            string programName = Assembly.GetExecutingAssembly().FullName.Split(',')[0];
+            Console.WriteLine($"\t{programName} Part 1 : {Part1(lines)}");
+            Console.WriteLine($"\t{programName} Part 2 : {Part2(lines)}");
+            Console.WriteLine();
+        }
+
+        public static int Part1(string[] lines)
+        {
+            return 0;
+        }
+        public static int Part2(string[] lines)
+        {
+            return 0;
+        }
+    }
+}
