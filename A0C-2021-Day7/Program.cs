2d0161d78985ec2e135d289b4b665e43b1c9da0d
Family <alistairfrith@gmail.com>
2021-12-08 22:01:58 +0000

    Day 7 done

--- A0C-2021-Day7/Program.cs
+++ A0C-2021-Day7/Program.cs
@@ -25,18 +25,57 @@ namespace A0C_2021_Day7
             Console.WriteLine($"Processing input from {inputFileName}");
 
             string programName = Assembly.GetExecutingAssembly().FullName.Split(',')[0];
-            Console.WriteLine($"\t{programName} Part 1 : {Part1(lines[0], 80)}");
-            Console.WriteLine($"\t{programName} Part 2 : {Part2(lines[0], 256)}");
+            var costs = Part1(lines[0]);
+            Console.WriteLine($"\t{programName} Part 1 : {costs.Item1}");
+            Console.WriteLine($"\t{programName} Part 2 : {costs.Item2}");
             Console.WriteLine();
         }
 
-        public static int Part1(string line, int numGenerations)
+        public static Tuple<int,int> Part1(string line)
         {
-            return 0;
+            int[] crabPositions = line.Split(',').Select(int.Parse).ToArray();
+
+            int min = crabPositions.Min();
+            int max = crabPositions.Max();
+
+            int minCost1 = int.MaxValue;
+            int minCost2 = int.MaxValue;
+            for (int targetPos = min; targetPos <= max; targetPos++)
+            {
+                int cost1 = TotalCost1(crabPositions, targetPos);
+                minCost1 = Math.Min(minCost1, cost1);
+
+                int cost2 = TotalCost2(crabPositions, targetPos);
+                minCost2 = Math.Min(minCost2, cost2);
+
+                Console.WriteLine($"Position {targetPos} costs {cost1} or {cost2}");
+            }
+
+            return new Tuple<int, int>(minCost1, minCost2);
         }
 
+        private static int TotalCost2(int[] crabPositions, int targetPos)
+        {
+            return crabPositions.Select(c => Accumulate(Math.Abs(targetPos - c))).Sum();
+        }
 
-        public static int Part2(string line, int numGenerations)
+        private static int Accumulate(int steps)
+        {
+            int total = 0;
+
+            while (steps > 0)
+            {
+                total += steps--;
+            }
+            return total;
+        }
+
+        private static int TotalCost1(int[] crabPositions, int targetPos)
+        {
+            return crabPositions.Select(c => Math.Abs(targetPos - c)).Sum();
+        }
+
+        public static int Part2(string line)
         {
             return 0;
         }
