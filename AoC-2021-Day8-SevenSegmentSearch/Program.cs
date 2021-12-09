06805db1f57c2674a7b9ab131cf058d7b0c8d868
Family <alistairfrith@gmail.com>
2021-12-09 19:48:52 +0000

    Day 8 abandoned (did part one in Excel but can't figure out part 2)

--- AoC-2021-Day8-SevenSegmentSearch/Program.cs
+++ AoC-2021-Day8-SevenSegmentSearch/Program.cs
@@ -37,7 +37,54 @@ namespace AoC_2021_Day8_SevenSegmentSearch
         }
         public static int Part2(string[] lines)
         {
+            foreach(string line in lines)
+            {
+               
+                int value = Decode(line);
+            }
             return 0;
+        }
+
+        private static int Decode(string line)
+        {
+            string[] parts = line.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
+            string[] readings = parts[0].Split(new char[] { ' ' });
+            string[] digitCodes = new string[10];
+
+            digitCodes[1] = readings.Where(r => r.Length == 2).First();
+            digitCodes[4] = readings.Where(r => r.Length == 4).First();
+            digitCodes[7] = readings.Where(r => r.Length == 3).First();
+            digitCodes[8] = readings.Where(r => r.Length == 7).First();
+
+            //   0
+            // 1   2
+            //   3
+            // 4   5
+            //   6
+
+            char[] segmentCodes = new char[7];
+            segmentCodes[0] = UniqueChars(new List<string>() { digitCodes[1], digitCodes[7]}).First();
+
+
+            throw new NotImplementedException();
+        }
+
+        private static List<char> UniqueChars(List<string> list)
+        {
+            List<char> allCodes = new List<char>();
+
+            foreach (string s in list)
+            {
+                allCodes.AddRange(s.ToList());
+            }
+
+            var result =
+                from x in allCodes
+                group allCodes by x into grp
+                where grp.Count() == 1
+                select grp.Key;
+
+            return result.ToList() ;
         }
     }
 }
